7e05383286936393b10c4d4796a75eff10416530
Biazinhasz <110834788+Biazinhasz@users.noreply.github.com>
2022-09-05 10:57:27 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int n;
+            n = int.Parse (Console.ReadLine ());
+            
+            if (n%2 == 0)
+            {
+                Console.WriteLine("par");
+            }
+            else
+            {
+                Console.WriteLine("impar");
+            }
+        }
+    }
+}
